a4ad5ec07ef2aba6dbb6d020d405d602365cafbf
Umair Faheem <103030494+UmairFaheem042@users.noreply.github.com>
2023-05-24 11:23:32 +0530

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-const primaryNav = document.querySelector('.nav-link');
-const navToggle = document.getElementById('menu-icon');
-
-navToggle.addEventListener('click', ()=> {
-  primaryNav.classList.toggle("active");
-})
